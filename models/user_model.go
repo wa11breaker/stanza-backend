15dcb904ff836510dea5686f3536a6a7cf83ae7b
Akshay <akshayasok1998@gmail.com>
2022-10-08 20:56:34 +0530

    feat: user CRUD

--- models/user_model.go
+++ models/user_model.go
@@ -3,8 +3,7 @@ package models
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type User struct {
-	Id       primitive.ObjectID `json:"id,omitempty"`
-	Name     string             `json:"name,omitempty" validate:"required"`
-	Location string             `json:"location,omitempty" validate:"required"`
-	Title    string             `json:"title,omitempty" validate:"required"`
+	Id        primitive.ObjectID `json:"id,omitempty"`
+	Name      string             `json:"name,omitempty" validate:"required"`
+	AvatarUrl string             `json:"avatarUrl,omitempty"`
 }
--- responses/user_response.go
+++ responses/user_response.go
@@ -1,7 +1,7 @@
 package responses
 
 type UserResponse struct {
-	Status  int                    `json:"status"`
-	Message string                 `json:"message"`
-	Data    map[string]interface{} `json:"data"`
+	Status  int         `json:"status"`
+	Message string      `json:"message"`
+	Data    interface{} `json:"data"`
 }
